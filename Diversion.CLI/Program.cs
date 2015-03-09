5cf8b31771c829512e39877dfbbb16f1f5e29b22
Michael Melancon <michaelmelancon@gmail.com>
2015-03-09 18:58:47 -0500

    Made progress on CLI.

--- Diversion.CLI/Program.cs
+++ Diversion.CLI/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Microsoft.Build.Evaluation;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Diversion.CLI
 {
@@ -6,7 +10,81 @@ namespace Diversion.CLI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(new NextVersion().Determine(args[0], args[1]));
+            var options = CmdLine.CommandLine.Parse<Options>();
+            options.ProjectFile = options.ProjectFile ?? GetProjectFilePath();
+            options.ReleasedPath = options.ReleasedPath ?? GetReleasedPath(options.ProjectFile);
+            options.ProposedPath = options.ProposedPath ?? GetProposedPath(options.ProjectFile);
+            var diversion = new AssemblyDiversionDiviner().Divine(options.ReleasedPath, options.ProposedPath);
+            var version = new NextVersion().Determine(diversion);
+            if (version > diversion.New.Version)
+                if (!options.Correct)
+                {
+                    CmdLine.CommandLine.WriteLineColor(ConsoleColor.DarkRed, "v{0} [Currently v{1}]", version, diversion.New.Version);
+                }
+                else
+                {
+                    CmdLine.CommandLine.WriteLineColor(ConsoleColor.DarkCyan, "v{0} [Was v{1}]", version, diversion.New.Version);
+                }
+            else if (version == diversion.New.Version)
+                CmdLine.CommandLine.WriteLineColor(ConsoleColor.Green, "v{0}", version);
+            else
+                CmdLine.CommandLine.WriteLineColor(ConsoleColor.DarkGreen, "v{0} [Determined v{1}]", diversion.New.Version, version);
+            Console.ReadKey();
         }
+
+        private static string GetProposedPath(string projectFile)
+        {
+            ProjectCollection projects = new ProjectCollection(new Dictionary<string, string> {{ "Configuration", "Release" }});
+            var project = projects.LoadProject(projectFile);
+            project.Build();
+            return Path.Combine(project.DirectoryPath, project.GetPropertyValue("OutputPath"), project.GetPropertyValue("TargetFileName"));
+        }
+
+        private static string GetReleasedPath(string projectFile)
+        {                        
+            ProjectCollection projects = new ProjectCollection(new Dictionary<string, string> { { "Configuration", "Release" } });
+            var project = projects.LoadProject(projectFile);
+            project.Build();
+            return Path.Combine(project.DirectoryPath, project.GetPropertyValue("OutputPath"), project.GetPropertyValue("TargetFileName"));
+        }
+
+        private static string GetProjectFilePath()
+        {
+            string[] projectExtensions = new[] { ".csproj", ".vbproj", ".proj" };
+            var projectFile = Directory.EnumerateFiles(Environment.CurrentDirectory).Select(file => new FileInfo(file)).FirstOrDefault(file => file.Name.Equals(file.Directory.Name) && projectExtensions.Contains(file.Extension)) ??
+                Directory.EnumerateFiles(Environment.CurrentDirectory).Select(file => new FileInfo(file)).FirstOrDefault(file => projectExtensions.Contains(file.Extension));
+            return projectFile == null ? null : projectFile.FullName;
+        }
+
+
+    }
+
+    class Options
+    {
+        [CmdLine.CommandLineParameter(Name = "ProposedPath", ParameterIndex = 1)]
+        public string ProposedPath { get; set; }
+
+        [CmdLine.CommandLineParameter("r", Name = "ReleasedPath")]
+        public string ReleasedPath { get; set; }
+
+        [CmdLine.CommandLineParameter("f", Name = "ProjectFile")]
+        public string ProjectFile { get; set; }
+
+        [CmdLine.CommandLineParameter("c", Name = "Correct")]
+        public bool Correct { get; set; }
+
+        [CmdLine.CommandLineParameter("a", Name = "AssemblyInfoFile", Default = "Properties\\AssemblyInfo.cs")]
+        public string AssemblyInfoFile { get; set; }
+
+        [CmdLine.CommandLineParameter("s", Name = "ReleaseSource", Default = ReleaseSource.Auto)]
+        public ReleaseSource ReleaseSource { get; set; }
+    }
+
+    enum ReleaseSource
+    {
+        Auto,
+        NuGet,
+        Git,
+        File        
     }
 }
--- Diversion.CLI/Properties/AssemblyInfo.cs
+++ Diversion.CLI/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Diversion.CLI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A command line interface for Diversion.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LocalMed")]
 [assembly: AssemblyProduct("Diversion.CLI")]
@@ -22,15 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("aeaf2077-dfa6-4e31-9d8c-914855c90998")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
